e711e64e1f73eff78234f62053c90557987f3e82
jinalee <spzn02@gmail.com>
2020-05-09 17:07:35 +0900

    Update ListItem.vue

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -2,31 +2,13 @@
   <ol id="app" class="list" >
     <li v-for="item in list" :key=item.id>
       <b>{{item.time_ago}} </b>
-      <span>by <router-link v-bind:to="`/user/${item.user}`"> {{item.user}} </router-link> </span><br/>
+      <span v-if="item.user !== null">by <router-link v-bind:to="`/user/${item.user}`"> {{item.user}} </router-link> </span><br/>
       <a v-bind:href="item.url">{{item.title}}</a>
-      <router-link :to="`/item/${item.id}`">
+      <router-link v-if="item.comments_count !== 0" :to="`/item/${item.id}`">
           <i> ({{ item.comments_count }})</i>
       </router-link>
     </li>
   </ol>
-    <!-- <ol id="app" class="list">
-    <li v-for="item in list" :key=item.id>
-      <b>{{item.time_ago}} </b><br/>
-      <a v-bind:href="item.url">{{item.title}}</a>
-    </li>
-  </ol> -->
-  <!-- job -->
-    <!-- <ol id="app" class="list">
-    <li v-for="item in list" :key=item.id>
-      <b>{{item.time_ago}} </b>
-      <span>by {{item.user}}</span><br/>
-      <a v-bind:href="item.url">{{item.title}}</a>
-      <router-link :to="`/item/${item.id}`">
-          <i> ({{ item.comments_count }})</i>
-      </router-link>
-    </li>
-  </ol> -->
-  <!-- news -->
 </template>
 
 <script>
@@ -39,7 +21,8 @@ export default {
     created(){
         this.$store.dispatch('FETCH_LIST', this.$route.name);
         localStorage.setItem('nav', this.$route.name);
-    }
+    },
+
 }
 </script>
 
